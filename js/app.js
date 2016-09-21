52a9c90b7f02399afc69f70bf6ad58a383008afa
clurdablur <coucc15@gmail.com>
2016-09-21 05:44:12 -0500

    Added less & babel

---
+++ js/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    var Wine = {
+        currentQuestion: 0,
+        score: 0
+    };
+
+    var wine = Object.create(Wine);
+    var currentQuestion = 0;
+    var score = 0;
+
+
+    wine.questions =[{
+        main: "Which country is the world’s biggest red wine consumers?",
+        answers: ['Italy', 'China', 'USA', 'France'],
+        correctAnswer: 1,
+        explaination: ""
+    }, {
+        main: "Why do women get drunk faster from wine than men?",
+        answers: ['Smaller', 'Weigh less', 'Higher fat content', 'Drink more'],
+        correctAnswer: 2,
+        explaination: ""
+    }, {
+        main: "Which of these is NOT a type of wine?",
+        answers: ['Cabernet', 'Brandy', 'Port', 'Rose'],
+        correctAnswer: 1,
+        explaination: ""
+    }, {
+        main: "What kind of wine contains the most antioxidants?",
+        answers: ['Sangria', 'White wine', 'Rose', 'Red wine'],
+        correctAnswer: 3,
+        explaination: ""
+    }, {
+        main: "How long does an opened bottle of red wine last?",
+        answers: ['24 hours', '1-2 days', '3-5 days', 'up to 10 days'],
+        correctAnswer: 2,
+        explaination: "Red wine last 3-5 days in a cool dark place with a cork. Sparkling wine lasts the least after being opened and is only good for 1-3 days in the fridge. Fortified wine and Bag-in-a-Box wine last the longest up to 28 days if stored properly."
+    }];
+
+    $('#nextquestion').click(function() {
+        var userInput = $('input:checked').val();
+        if (!userInput){
+            alert("Answer Question")
+            return
+        } 
+        if (wine.questions[currentQuestion].correctAnswer == userInput) {
+            score++;
+        }
+        currentQuestion++;
+        if (currentQuestion == wine.questions.length) {
+            $('#score_wrapper span').text(score);
+            $('#question_wrapper').hide();
+            $('#score_wrapper').show();
+
+        }
+        else updateView();
+    });
+    $('#restartquiz').click(function() {
+        currentQuestion = 0;
+        score = 0;
+        updateView();
+    });
+    function updateView() {
+        $('#questionMain').html(wine.questions[currentQuestion].main);
+        $('#question_wrapper').show();
+        $('#score_wrapper').hide();      
+    
+        var radios = $('#questionAnswer span');
+        for (i = 0; i < 4; i++) {
+            var radio = $(radios[i]);
+            radio.text(wine.questions[currentQuestion].answers[i]);
+        }
+
+
+    };
+updateView();
+
+});
